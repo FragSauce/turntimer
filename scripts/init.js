4007dd4eab0cc948d5c9856c61862e61a0491ec6
LokeLE <ll@admill.dk>
2021-10-29 21:14:20 +0200

    v1.0.4

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,6 @@
 console.log("hello world")
+
+
+Hooks.on('updateCombatant', function() {
+    console.log("turn changed in innit")
+});
--- scripts/timer.js
+++ scripts/timer.js
@@ -1,18 +1,9 @@
 
 
 
-
-
-
-
-async function updateCombatantEvent(combat, _update, _options, _userId) {
-    
+Hooks.on('updateCombatant', function() {
     console.log("turn changed")
-
-}
-
-
-Hooks.on('updateCombatant', updateCombatantEvent);
+});
 
 Hooks.on("ready", function() {
     console.log("This code runs once core initialization is ready and game data is available. 33333333333333333333333333333333333333333333333");
